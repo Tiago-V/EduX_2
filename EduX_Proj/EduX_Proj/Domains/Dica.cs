c759cc747bf767cbe14fc15646658ab4483ccb25
Tiago Valim <tiago.valim@gmail.com>
2020-09-29 11:36:56 -0300

    gitignore adicionado

---
+++ EduX_Proj/EduX_Proj/Domains/Dica.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace EduX_Proj.Domains
+{
+    public partial class Dica
+    {
+        public Dica()
+        {
+            Curtida = new HashSet<Curtida>();
+        }
+
+        public int IdDica { get; set; }
+        public string Texto { get; set; }
+        public int IdUsuario { get; set; }
+        public IFormFile Imagem { get; set; }
+
+        public string UrlImagem { get; set; }
+
+
+
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+        public virtual ICollection<Curtida> Curtida { get; set; }
+    }
+}
---
+++ EduX_Proj/EduX_Proj/Interfaces/IObjetivoAluno.cs
@@ -0,0 +1,17 @@
+﻿using EduX_Proj.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduX_Proj.Interface
+{
+    interface IObjetivoAluno
+    {
+        List<ObjetivoAluno> Listar();
+        ObjetivoAluno BuscarPorID(int id);
+        void Adicionar(ObjetivoAluno ObjetivoA);
+        void Alterar(int id, ObjetivoAluno ObjetivoA);
+        void Excluir(int id);
+    }
+}
