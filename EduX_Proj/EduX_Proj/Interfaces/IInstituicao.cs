a6ed5c2a3f797a67d9471fba6a4a2dbdb065dc6e
Tiago Valim <tiago.valim@gmail.com>
2020-09-30 12:35:22 -0300

    Instituicao Interface, Repository, Controller Criado

---
+++ EduX_Proj/EduX_Proj/Interfaces/IInstituicao.cs
@@ -0,0 +1,17 @@
+﻿using EduX_Proj.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduX_Proj.Interfaces
+{
+    interface IInstituicao
+    {
+        List<Instituicao> ListarTodos();
+        Instituicao BuscarPorID(int id);
+        void Adicionar(Instituicao i);
+        void Alterar(int id, Instituicao i);
+        void Remover(int id);
+    }
+}
---
+++ EduX_Proj/EduX_Proj/Repositories/InstituicaoRepository.cs
@@ -0,0 +1,89 @@
+﻿using EduX_Proj.Contexts;
+using EduX_Proj.Domains;
+using EduX_Proj.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduX_Proj.Repositories
+{
+    public class InstituicaoRepository : IInstituicao
+    {
+        private readonly EduXContext _ctx;
+
+        public InstituicaoRepository()
+        {
+            _ctx = new EduXContext();
+        }
+
+        public void Adicionar(Instituicao i)
+        {
+            _ctx.Instituicao.Add(i);
+            _ctx.SaveChanges();
+        }
+
+        public void Alterar(int id, Instituicao i)
+        {
+            try
+            {
+                Instituicao instituicao = BuscarPorID(id);
+
+                instituicao.Nome        = i.Nome;
+                instituicao.Logradouro  = i.Logradouro;
+                instituicao.Numero      = i.Numero;
+                instituicao.Complemento = i.Complemento;
+                instituicao.Bairro      = i.Bairro;
+                instituicao.Cidade      = i.Cidade;
+                instituicao.Uf          = i.Uf;
+                instituicao.Cep         = i.Cep;
+
+                _ctx.Instituicao.Update(instituicao);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Instituicao BuscarPorID(int id)
+        {
+            try
+            {
+                return _ctx.Instituicao.Find(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Instituicao> ListarTodos()
+        {
+            try
+            {
+                return _ctx.Instituicao.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Remover(int id)
+        {
+            try
+            {
+                Instituicao instituicao = BuscarPorID(id);
+
+                _ctx.Instituicao.Remove(instituicao);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
