3fae1c9df6c4a0c3447c9f4ac4c0b45dd6d8cf0c
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-09-30 21:17:51 -0300

    Controllers categoria e curso criados - Amanda

---
+++ EduX_Proj/EduX_Proj/Controllers/CursoController.cs
@@ -0,0 +1,138 @@
+﻿using Amazon.DynamoDBv2.Model;
+using EduX_Proj.Domains;
+using EduX_Proj.Interfaces;
+using EduX_Proj.Repositories;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduX_Proj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+        private readonly ICurso _curso;
+
+        public CursoController()
+        {
+            _curso = new CursoRepository();
+        }
+
+        /// <summary>
+        /// Lista dos Cursos
+        /// </summary>
+        /// <returns>Lista os Cursos</returns>
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var cursos = _curso.ListarTodos();
+
+                if (cursos.Count == 0)
+                    return NoContent();
+                return Ok(cursos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Busca um Curso por seu ID
+        /// </summary>
+        /// <param name="id">Id Curso</param>
+        /// <returns>Curso</returns>
+
+        [HttpGet]
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                Curso curso = _curso.BuscarPorId(id);
+                if (curso == null)
+                    return NotFound();
+                return Ok(curso);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Adiciona um Curso
+        /// </summary>
+        /// <param name="curso">Curso</param>
+        /// <returns>Status: Ok</returns>
+
+        [HttpPost]
+        public IActionResult Post(Curso curso)
+        {
+            try
+            {
+                _curso.Adicionar(curso);
+
+                return Ok(curso);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Altera elementos do Curso
+        /// </summary>
+        /// <param name="id">Id do Curso que será alterado</param>
+        /// <param name="curso">Curso</param>
+        /// <returns>Status: Ok</returns>
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, Curso curso)
+        {
+            try
+            {
+                _curso.Alterar(id, curso);
+
+                return Ok(curso);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Remove um Curso
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Status: Ok</returns>
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                var curso = _curso.BuscarPorId(id);
+
+                if (curso == null)
+                    return NotFound();
+                _curso.Remover(id);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ EduX_Proj/EduX_Proj/Repositories/CursoRepository.cs
@@ -0,0 +1,79 @@
+﻿using EduX_Proj.Contexts;
+using EduX_Proj.Domains;
+using EduX_Proj.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduX_Proj.Repositories
+{
+    public class CursoRepository : ICurso
+    {
+        private readonly EduXContext _ctx;
+
+        public CursoRepository()
+        {
+            _ctx = new EduXContext();
+        }
+        public void Adicionar(Curso curso)
+        {
+            _ctx.Curso.Add(curso);
+            _ctx.SaveChanges();
+        }
+
+        public void Alterar(Guid id, Curso curso)
+        {
+            try
+            {
+                Curso cso = BuscarPorId(id);
+                cso.Titulo = curso.Titulo;
+
+                _ctx.Curso.Update(curso);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Curso BuscarPorId(Guid id)
+        {
+            try
+            {
+                return _ctx.Curso.Find(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Curso> ListarTodos()
+        {
+            try
+            {
+                return _ctx.Curso.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Remover(Guid id)
+        {
+            try
+            {
+                Curso cso = BuscarPorId(id);
+                _ctx.Curso.Remove(cso);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
